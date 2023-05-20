4a2255e389c4233a0015ba0d4284461d06f25939
ksh <alexuhyun@gmail.com>
2023-05-20 19:03:14 +0900

    Implement a function to get a word at a given range

--- content.js
+++ content.js
@@ -1,1 +1,26 @@
-// WIP
+let currentWord = "";
+
+// Reference: https://stackoverflow.com/a/49961880
+const getWordAtRange = (range) => {
+  if (range.startContainer.nodeType !== Node.TEXT_NODE) {
+    return null;
+  }
+  range.expand("word");
+
+  const word = range.toString().trim();
+  if (word === "") {
+    return null;
+  }
+  if (!/^[A-Za-z]+$/.test(word)) {
+    return null;
+  }
+  if (word === currentWord) {
+    return currentWord;
+  }
+  return word;
+};
+
+document.addEventListener("mousemove", function (event) {
+  const range = document.caretRangeFromPoint(event.clientX, event.clientY);
+  const word = getWordAtRange(range);
+});
